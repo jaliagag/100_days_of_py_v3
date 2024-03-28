92188bd81c6fff75d9608e47f8379ba8f0ad9d32
Jose Aliaga <jmfaliaga@gmail.com>
2024-03-28 00:34:24 -0300

    day 20: starting fibo sequence

--- mouredev/logicos/001.py
+++ mouredev/logicos/001.py
@@ -17,7 +17,5 @@ for i in range(1,101):
     if i % 5 == 0:
         print(i,"buzz")
         continue
-        #if i % 5 == 0 and i % 3 != 0:
-        #    print(i,"buzz")
     else:
         print(i)
---
+++ mouredev/logicos/003.py
@@ -0,0 +1,16 @@
+#/*
+# * Escribe un programa que imprima los 50 primeros números de la sucesión
+# * de Fibonacci empezando en 0.
+# * - La serie Fibonacci se compone por una sucesión de números en
+# *   la que el siguiente siempre es la suma de los dos anteriores.
+# *   0, 1, 1, 2, 3, 5, 8, 13...
+# */
+
+def fibo(a: int, b: int):
+    return a + b
+
+for i in range(1,51):
+    fibo(i, i)
+
+
+
