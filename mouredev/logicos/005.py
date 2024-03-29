d0a45ddc731c8c33f191a1a2c4acca06e5fb0735
Jose Aliaga <jmfaliaga@gmail.com>
2024-03-29 11:45:08 -0300

    day 22: starting polygon analisys

---
+++ mouredev/logicos/005.py
@@ -0,0 +1,19 @@
+#/*
+# * Crea una única función (importante que sólo sea una) que sea capaz
+# * de calcular y retornar el área de un polígono.
+# * - La función recibirá por parámetro sólo UN polígono a la vez.
+# * - Los polígonos soportados serán Triángulo, Cuadrado y Rectángulo.
+# * - Imprime el cálculo del área de un polígono de cada tipo.
+# */
+
+def area(poligono: str):
+    lowerme = poligono.lower()
+    if lowerme == "triangulo":
+
+        return True
+    elif lowerme == "cuadrado":
+        return True
+    elif lowerme == "rectangulo":
+        return True
+    else:
+        return False
