5edc5084a1881d29165e27081760766bd4a5d254
Jose Aliaga <jmfaliaga@gmail.com>
2024-03-30 11:57:21 -0300

    day 23: polygon calc

--- mouredev/logicos/005.py
+++ mouredev/logicos/005.py
@@ -6,14 +6,31 @@
 # * - Imprime el cálculo del área de un polígono de cada tipo.
 # */
 
-def area(poligono: str):
+def area(poligono: str, data1: int, data2: int):
     lowerme = poligono.lower()
     if lowerme == "triangulo":
+        base = data1
+        height = data2
+        return (data1 * data2) * 0.5
 
-        return True
     elif lowerme == "cuadrado":
-        return True
+        lado = data1
+        lado2 = data2
+        if lado != lado2:
+            print("No es un cuadrado! es un rectangulo - para que sea un cuadrado, ambos datos/lados deben ser iguales")
+            return False
+        return lado * lado2
+
     elif lowerme == "rectangulo":
-        return True
+        length = data1
+        width = data2
+        return data1 * data2
+
     else:
         return False
+
+print(area("cuadrado", 2,2))
+print(area("cuadrado", 2,3))
+print(area("triangulo", 20,10))
+print(area("rectangulo", 4,2))
+print(area("cualca", 4,2))
