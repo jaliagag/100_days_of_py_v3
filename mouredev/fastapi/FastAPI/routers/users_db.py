475010e4d43124ed399a46c1b6290743ed75e77e
Jose Aliaga <jmfaliaga@gmail.com>
2024-04-16 08:35:59 -0300

    day 37: added 2 users! weird missing field in User model?

--- mouredev/fastapi/FastAPI/routers/users_db.py
+++ mouredev/fastapi/FastAPI/routers/users_db.py
@@ -4,43 +4,25 @@ from db.schemas.user import user_schema
 from db.client import db_client
 
 
-from fastapi.responses import FileResponse
-
 router = APIRouter(
                 prefix="/userdb",
                 tags=["userdb"],
                 responses={status.HTTP_404_NOT_FOUND: {"message": "no encontrado"}}
                 )
 
-# uvicorn users:router --reload
-
-@router.get("/favicon.ico", include_in_schema=False)
-async def favicon():
-    return FileResponse(favicon_path)
-
-
-
 user_list = []
-
-
-@router.get("/")
-async def users():
-    return user_list
-
-@router.get("/{id}")
-async def user(id: int): # query
-    return search_user(id)
 
 @router.post("/", response_model=User, status_code=status.HTTP_201_CREATED)
 async def create_user(user: User):
+
     user_dict = dict(user)
     del user_dict["id"]
 
     id = db_client.local.users.insert_one(user_dict).inserted_id
 
-    new_user = user_schema(db_client.local.find_one({"_id": id}))
+    new_user = user_schema(db_client.local.users.find_one({"_id": id}))
 
-    return User(**new_user) # ** para indicar que le paso todos los campos 
+    return User(**new_user) # ** para indicar que le paso todos los campos
 
 
 @router.put("/")
